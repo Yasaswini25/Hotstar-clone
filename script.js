3dbc6e9965fed4dcbfa7844cc64050b7253d52a2
Yasaswini25 <99401232+Yasaswini25@users.noreply.github.com>
2023-07-20 21:12:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,27 @@ let movies = [
  {
   name: "Loki",
   des : "The mercurial villain Loki resumes his role as the God of Mischief in a new series that takes place after the events of “Avengers: Endgame.”",
-  image:"D:/projects and internships/hotstar clone/images and videos/slider1.PNG" 
+  image:images and videos/slider1.PNG 
   },
   {
     name: "Falcon and the Winter Solider",
     des : "Following the events of 'Avengers: Endgame,' Sam Wilson/Falcon and Bucky Barnes/Winter Soldier team up in a global adventure that tests their abilities -- and their patience.",
-    image: "D:/projects and internships/hotstar clone/images and videos/slider2.PNG"
+    image: images and videos/slider2.PNG
   },
   {
     name: "Wanda Vision",
     des : "Blends the style of classic sitcoms with the MCU, in which Wanda Maximoff and Vision - two super-powered beings living their ideal suburban lives - begin to suspect that everything is not as it seems.",
-    image: "D:/projects and internships/hotstar clone/images and videos/slider3.PNG"
+    image: images and videos/slider3.PNG
   },
   {
     name: "Raya and the last dragon",
     des : "In a realm known as Kumandra, a re-imagined Earth inhabited by an ancient civilization, a warrior named Raya is determined to find the last dragon.",
-    image: "D:/projects and internships/hotstar clone/images and videos/slider4.PNG"
+    image: images and videos/slider4.PNG
   },
   {
     name: "Luca",
     des : "On the Italian Riviera, an unlikely but strong friendship grows between a human being and a sea monster disguised as a human.",
-    image:"D:/projects and internships/hotstar clone/images and videos/slider5.PNG" 
+    image:images and videos/slider5.PNG" 
   }
 ];
       
